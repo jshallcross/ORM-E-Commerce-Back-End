1b900390b4c6caba6c1ab42d400188bda53dd169
Jon Shallcross <jshallcross1@icloud.com>
2021-03-06 11:00:07 -0500

    coded server.js to sync sequelize models

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
-
+/*
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -11,5 +11,19 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
+*/
+    let sequelize;
+
+    if (process.env.JAWSDB_URL) {
+        sequelize = new Sequelize(process.env.JAWSDB_URL);
+    } else {
+        sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+            host: 'localhost',
+            dialect: 'mysql',
+            dialectOptions:{
+              decimalNumbers: true,
+            },
+        });
+    }
 
 module.exports = sequelize;
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -9,6 +9,7 @@ ProductTag.init(
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      primaryKey: true,
       autoIncrement: true,
     },
     product_id: {
