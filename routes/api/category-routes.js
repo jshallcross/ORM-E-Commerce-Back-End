225f6d67fd6658ba40e368c4379388dc07306e19
Jon Shallcross <jshallcross1@icloud.com>
2021-03-06 11:52:17 -0500

    api routes completed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,25 +7,59 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    
-  })
+      include: [Product],
+  }).then((dbCategory) => {
+      res.json(dbCategory);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include:[Product],
+  }).then((dbCategory) => {
+      res.json(dbCategory);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    id: req.params.id,
+    category_name: req.body.category_name
+  }).then((dbCategory) => {
+    res.json(dbCategory);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }).then((dbCategory) => {
+      res.json(dbCategory);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({ 
+      where: {
+        id: req.params.id
+      }
+  }).then((dbCategory) => {
+      res.json(dbCategory);
+  });
 });
 
 module.exports = router;
